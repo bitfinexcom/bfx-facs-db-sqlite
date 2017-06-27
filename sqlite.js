703ce12f18f63cd8a3ca18240fe3f595d0834d70
Robert Kowalski <rok@kowalski.gd>
2017-06-27 13:16:05 +0100

    core: throw if directory does not exists
    previously the service just stopped working / callback was
    never called

--- sqlite.js
+++ sqlite.js
@@ -3,6 +3,8 @@
 const async = require('async')
 const SqliteDb = require('sqlite3')
 const Facility = require('./base')
+const path = require('path')
+const fs = require('fs')
 
 class Sqlite extends Facility {
   constructor (caller, opts, ctx) {
@@ -17,8 +19,15 @@ class Sqlite extends Facility {
     async.series([
       next => { super._start(next) },
       next => {
+        const db = `${__dirname}/../../db/${this.name}_${this.opts.name}_${this.opts.label}.db`
+        const dbDir = path.dirname(db)
+
+        if (!fs.existsSync(dbDir)) {
+          throw new Error(`directory ${dbDir} does not exist`)
+        }
+
         this.db = new SqliteDb.Database(
-          `${__dirname}/../../db/${this.name}_${this.opts.name}_${this.opts.label}.db`,
+          db,
           next
         )
       }
